f8c4f46fdcbed2d1cc00ba8092c05ea493a43d8a
greg-suehr <gregory.suehr@gmail.com>
2019-11-25 09:53:33 -0800

    cleaning house in scripts/

--- scripts/count_all_playlist_tracks.py
+++ scripts/count_all_playlist_tracks.py
@@ -61,6 +61,43 @@ def count_all_playlist_tracks(user):
     return score
 
 
+def get_all_playlist_tracks( user, pid ):
+    """builds list of (track, artist) of playlist uri"""
+
+    response = sp.user_playlist_tracks(user, playlist_id = pid)
+
+    tracks = [ ( r['track']['name'], r['track']['artists'][0]['name'] )
+               for r in response['items'] ]
+
+    if len(tracks) == response['total']:
+        return tracks
+    else:
+        while len(tracks) < response['total']:
+            response = sp.user_playlist_tracks(user, playlist_id = pid, offset=len(tracks))
+            tracks += [ ( r['track']['name'], r['track']['artists'][0]['name'] )
+                        for r in response['items'] ]
+
+    return tracks
+
+
+def get_all_user_playlists( user, limit=50):
+    """returns a list of user-made playlist pids"""
+
+    nams = []
+    pids = []
+    offset = 0
+    response = {'items': range(limit)}
+    while len( response['items'] ) >= limit:
+        response = sp.user_playlists(user, offset=offset, limit=limit)
+
+        pids += [ item['uri'] for item in response['items'] if item['owner']['id'] == user ]
+        nams += [ item['name'] for item in response['items'] if item['owner']['id'] == user ]
+        offset += limit
+
+    return pids, nams
+    for i in range(len(pids)):
+        print(pids[i], nams[i])
+
 if __name__ == "__main__":
     user = 'gsuehr'
     token = util.prompt_for_user_token(user, 'playlist-read-private')
