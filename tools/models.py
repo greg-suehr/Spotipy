5f6ed86c7e3ab293014ce74b17b66036d1513652
gregsuehr <dev@null.com>
2021-04-24 09:24:36 -0400

    cleaning up dead files

--- tools/models.py
+++ tools/models.py
@@ -1,11 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+
+
+class User(models.Model):
+    uri  = models.CharField(max_length = 255, primary_key = True)
+    name = models.CharField(max_length = 31) # Spotify's limit is 30
+    last_login = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
 
 class Library(models.Model):
-    username  = models.CharField(max_length = 32) # Spotify's limit is 30
-    created   = models.DateTimeField(auto_now = True)
-    updated   = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{}'s Library".format(self.username)
-    
+
+
+class Playlist(models.Model):
+    uri  = models.CharField(max_length = 255, primary_key = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
